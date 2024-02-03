3827462d7031f14523316459d2706754987d17aa
younes-uca <y.zouani@uca.ma>
2024-02-03 13:29:32 +0100

    init

---
+++ frontend/src/app/module/admin/view/commun/commun-admin-routing.module.ts
@@ -0,0 +1,39 @@
+
+// const root = environment.rootAppUrl;
+
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AuthGuard } from 'src/app/controller/guards/auth.guard';
+
+
+
+import { ProduitListAdminComponent } from './produit/list/produit-list-admin.component';
+@NgModule({
+    imports: [
+        RouterModule.forChild(
+            [
+                {
+                    path: '',
+                    children: [
+
+
+                        {
+
+                            path: 'produit',
+                            children: [
+                                {
+                                    path: 'list',
+                                    component: ProduitListAdminComponent ,
+                                    canActivate: [AuthGuard]
+                                }
+                            ]
+                        },
+
+                    ]
+                },
+            ]
+        ),
+    ],
+    exports: [RouterModule],
+})
+export class CommunAdminRoutingModule { }
